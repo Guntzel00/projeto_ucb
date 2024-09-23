e74de71b388ba031a3e53f2f3bf62574d6ce3c18
Arthur Ferreira <arthur203teixeira@gmail.com>
2024-09-23 16:27:12 -0300

    adionado pagina inicial, em desenvolvimento

--- src/pages/pagina_login/Pagina_login.jsx
+++ src/pages/pagina_login/Pagina_login.jsx
@@ -1,8 +1,18 @@
-import Header_logon from '../../components/header/Header_logon';
+import foto1 from '../../assets/msgdeboasvindas.svg';
+import foto2 from '../../assets/bonecos.svg';
+import Footer from '../../components/footer/footer';
 import './Pagina_login.css';
 
+
 function Pagina_login() {
-	return <></>;
+	return (<> 
+        <header_inicial></header_inicial>
+        <div>
+            <img src={foto1} alt="mensagem" />
+            <img src={foto2} alt="mensagem" />
+        </div>
+        <Footer />
+    </>);
 }
 
-export default Pagina_login;
+export default Pagina_login; 
