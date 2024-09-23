a599158eb247cb52f4da14f40b78ecf46eea428a
Arthur Ferreira <arthur203teixeira@gmail.com>
2024-09-23 17:10:38 -0300

    pagina login

--- src/pages/pagina_login/Pagina_login.jsx
+++ src/pages/pagina_login/Pagina_login.jsx
@@ -1,5 +1,6 @@
 import foto1 from '../../assets/msgdeboasvindas.svg';
 import foto2 from '../../assets/bonecos.svg';
+import foto3 from '../../assets/caixas.svg';
 import Footer from '../../components/footer/footer';
 import './Pagina_login.css';
 
@@ -9,8 +10,20 @@ function Pagina_login() {
         <header_inicial></header_inicial>
         <div>
             <img src={foto1} alt="mensagem" />
+
+            <form action="">
+            <h2>Login</h2>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required></input>
+            <label for="password">Senha:</label>
+            <input type="password" id="password" name="password" required></input>
+            <button type="submit">Entrar</button>
+            </form>
+            
+            <img src={foto3} alt="mensagem" />
             <img src={foto2} alt="mensagem" />
         </div>
+        
         <Footer />
     </>);
 }
