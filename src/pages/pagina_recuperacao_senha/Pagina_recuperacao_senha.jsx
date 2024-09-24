315f15a37576b144681c4858284eef92ede5adc2
Guntzel00 <gustavo.guntzel@outlook.com>
2024-09-24 13:34:49 -0300

    Adicionando estrutura da pagina de recuperação de senha

---
+++ src/pages/pagina_recuperacao_senha/Pagina_recuperacao_senha.jsx
@@ -0,0 +1,21 @@
+import Footer from '../../components/footer/footer';
+import './Pagina_recuperacao_senha.css';
+
+function Pagina_recuperacao_senha() {
+	// Pode codar javascript aqui
+
+	let exemplo = 'Exemplo de variável';
+
+	return (
+		<>
+			<header_inicial></header_inicial>
+			<div>
+				<h1>{exemplo}</h1>
+			</div>
+
+			<Footer />
+		</>
+	);
+}
+
+export default Pagina_recuperacao_senha;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Pagina_inicial from '../pages/pagina_inicial/Pagina_incial'; // Página i
 import Pagina_conteudos from '../pages/pagina_conteudos/Pagina_conteudos'; // Página de conteúdos
 import Pagina_login from '../pages/pagina_login/Pagina_login';
 import Pagina_artigo from '../pages/pagina_artigo/Pagina_artigo';
+import Pagina_recuperacao_senha from '../pages/pagina_recuperacao_senha/Pagina_recuperacao_senha';
 
 function AppRoutes() {
 	return (
@@ -11,6 +12,7 @@ function AppRoutes() {
 			<Route path='/home' element={<Pagina_inicial />} />
 			<Route path='/conteudos' element={<Pagina_conteudos />} />
 			<Route path='/conteudos/:id' element={<Pagina_artigo />} />
+			<Route path='/recuperacao' element={<Pagina_recuperacao_senha />} />
 		</Routes>
 	);
 }
