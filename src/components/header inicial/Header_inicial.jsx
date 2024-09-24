b7fb64d865335d8531d0d372c21467a118d07450
Guntzel00 <gustavo.guntzel@outlook.com>
2024-09-24 19:34:30 -0300

    Tela de login

--- src/components/header inicial/Header_inicial.jsx
+++ src/components/header inicial/Header_inicial.jsx
@@ -1,2 +1,22 @@
 import './Header_inicial.css';
+import { Link } from 'react-router-dom'; // Importa o Link do React Router
+import Btn from '../btn/Btn';
 
+function Header_inicial() {
+	return (
+		<>
+			<header>
+				{/* Substituindo o <a> por <Link> para navegação via React Router */}
+				<div className='texto'>
+					<Link to='/'>
+						<h1>Seja Doador.org</h1>
+					</Link>
+					<Btn label='Cadastre-se' referencia='/cadastro' />
+				</div>
+				<div className='linha'></div>
+			</header>
+		</>
+	);
+}
+
+export default Header_inicial;
--- src/pages/pagina_login/Pagina_login.jsx
+++ src/pages/pagina_login/Pagina_login.jsx
@@ -5,51 +5,69 @@ import Footer from '../../components/footer/footer';
 import './Pagina_login.css';
 import { Link } from 'react-router-dom';
 import Btn from '../../components/btn/Btn';
+import Header_inicial from '../../components/header inicial/Header_inicial';
 
 function Pagina_login() {
-	return (<> 
-        <header_inicial></header_inicial>
+	return (
+		<>
+			<Header_inicial />
 
-        <div class="foto1">
-            <img src={foto1} alt="mensagem" width="853" height="822"/>            
-        </div>
+			<div className='pag_login'>
+				<div className='bloco-1'>
+					<div className='foto1'>
+						<img src={foto1} alt='mensagem' width='853' height='822' />
+					</div>
+					<div className='form-container'>
+						<div className='container-login'>
+							<h2>Login</h2>
+							<label htmlFor='email'>Email:</label>
+							<input type='email' id='email' name='email' required></input>
+							<label htmlFor='password'>Senha:</label>
+							<input
+								type='password'
+								id='password'
+								name='password'
+								required
+							></input>
+							<p>
+								Não possui cadastro? Clique <Link to='/cadastro'>aqui</Link>
+							</p>
+							<Btn label='Entrar' referencia='/home' />
+						</div>
+					</div>
+				</div>
 
-        <div className="foto3">
-        <img src={foto3} alt="mensagem" width="1169" height="935" />
+				<div className='bloco-depoimentos'>
+					<img src={foto3} alt='mensagem' width='1169' height='935' />
 
-        <div className="overlay">
-        <h1>
-            <span className="frase1">Sua doação faz a diferença.</span>
-            <p></p>
-            <span className="frase2">Deixa que nós lidamos com a parte complicada.</span>
-        </h1> 
-        <Btn label="Cadastre-se" referencia="/cadastro"/>
-        </div>
-        </div>
+					<div className='overlay'>
+						<h1>
+							Sua doação faz a diferença.
+							<br />
+							<span>Deixa que nós lidamos com a parte complicada.</span>
+						</h1>
+						<br />
+						<Btn label='Cadastre-se' referencia='/cadastro' />
+					</div>
+				</div>
 
-        <div class="foto2">
-        <img src={foto2} alt="mensagem" width="693" height="597" />
-        </div>
+				<div className='ultimo-bloco'>
+					<div className='ultima-frase'>
+						<h1>
+							Doe sangue,
+							<br />
+							<span>vidas dependem da sua ação.</span>
+						</h1>
+					</div>
+					<div className='medicos'>
+						<img src={foto2} alt='mensagem' width='693' height='597' />
+					</div>
+				</div>
+			</div>
 
-        <div class="form-container">
-        <form action="">
-            <h2>Login</h2>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required></input>
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required></input>
-            <h1>Não possui cadastro? Clique <Link to="/cadastro"><span class="palavra">aqui</span></Link></h1>
-            <Btn label="Entrar" referencia="/home"/>
-            </form>
-        </div>
-        
-        <div class="ultima-frase">
-        <span class="doe-sangue">Doe sangue, </span> <br />
-        <span class="vidas-dependem">vidas dependem da sua ação.</span>
-        </div>
-        
-        <Footer />
-    </>);
+			<Footer />
+		</>
+	);
 }
 
-export default Pagina_login; 
+export default Pagina_login;
